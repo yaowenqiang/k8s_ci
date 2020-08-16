29d432ef47b85cecb75b30b8fc65cb0714b3d05f
yaowenqiang <yaowenqiang111@163.com>
2020-08-16 14:23:27 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+import "net/http"
+
+func main () {
+	http.HandleFunc("/", func(res http.ResponseWriter, req *http.Request) {
+		res.Write([]byte("Hello world"))
+	})
+
+	http.ListenAndServe(":8080", nil)
+}
